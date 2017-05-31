01323a6e31741ad1cd9923a7ff5bbba45333d83a
andriy <andriystasenko22@gmail.com>
2017-05-31 17:59:59 +0300

    first commit

--- app/js/comon.js
+++ app/js/comon.js
@@ -1,6 +1,84 @@
 $(document).ready(function() {
 
-	// слайдер клієнтів
+	$('#Grid').mixItUp();
+
+	var home_slider =$('.home_slider');
+	home_slider.owlCarousel({
+		items:1,
+		loop:true,
+		smartSpeed:550,
+		animateOut: 'fadeOut',
+		dots:true,
+		onChanged: function(event){
+			setTimeout(function(){
+				$('.owl-item').eq(event.item.index).find('.home_slid_item_text').addClass('animated fadeInUp').show();
+			},1000);
+		},
+		onChange: function(event){
+			$('.owl-item').find('.home_slid_item_text').hide().removeClass('animated fadeInUp');
+		},
+		onInitialized: function(event){
+			setTimeout(function(){
+				$('.owl-item').eq(event.item.index).find('.home_slid_item_text').addClass('animated fadeInUp').show();
+			},600);
+		}
+
+	});
+	
+	// анімація текста
+	home_slider.on('initialize.owl.carousel', function(event) {
+		setTimeout(function(){
+			$('.owl-item').find('.home_slid_item_text').addClass('animated fadeInUp').show();
+		},1000);
+	});
+
+	// слайдер "Наша команда"
+	var comand_slider = $('.comand_slider');
+	comand_slider.owlCarousel({
+		items:4,
+		loop:true,
+		margin: 45,
+		responsiveClass:true,
+		smartSpeed:550,
+		responsive:{
+			0:{
+				items:2,
+			},
+			768:{
+				items:3,
+			},
+			992:{
+				items:4,
+			}
+		}
+	});
+		// кнопки слайдер "Наша команда"
+		$('.btn_next').click(function() {
+			comand_slider.trigger('next.owl.carousel');
+		});
+		$('.btn_prev').click(function() {
+			comand_slider.trigger('prev.owl.carousel');
+		});
+
+	// слайдер "Отзывы"
+	var say_slider = $('.say_slider');
+	say_slider.owlCarousel({
+		loop:true,
+		// items: 2,
+		margin: 115,
+		smartSpeed:550,
+		responsiveClass:true,
+		responsive:{
+			0:{
+				items:1,
+			},
+			768:{
+				items:2,
+			}
+		}
+	});
+
+	// слайдер "Наши клиенты"
 	var clients_slider = $('.clients_slider');
 	clients_slider.owlCarousel({
 		loop:true, 
@@ -8,6 +86,7 @@ $(document).ready(function() {
 		autoplayTimeout:3000,
 		autoplaySpeed: 1000,
 		margin:90,
+		smartSpeed:550,
 		responsiveClass:true,
 		responsive:{
 			0:{
@@ -29,24 +108,6 @@ $(document).ready(function() {
 			}
 		}
 	});
-	
-	var comand_slider = $('.comand_slider');
-	comand_slider.owlCarousel({
-		items:4,
-		loop:true,
-		// nav:true,
-		margin: 35,
-		autoplay:true,
-		autoplayTimeout:3000,
-		autoplaySpeed: 1000,
-	});
-
-	$('.btn_next').click(function() {
-		comand_slider.trigger('next.owl.carousel');
-	})
-	$('.btn_prev').click(function() {
-		comand_slider.trigger('prev.owl.carousel');
-	})
 
 	// однакова висота блоків "Послуги"
 	var max_height=[];
@@ -62,11 +123,12 @@ $(document).ready(function() {
 	});
 
 	// таби 'Портфоліо'
-	$('.portfolio .portfolio_filter li a').click(function(event) {
+	$('.menu li a, .portfolio .portfolio_filter li a').click(function(event) {
 		event.preventDefault();
 		AddActive(this);
 	});
 
+	// анімація tab_layout "Портфоліо"
 	$('.tab_item').hover(function() {
 		$(this).find('.tab_layout .layout_icon li').first().children('a').addClass('animated fadeInUp');
 		$(this).find('.tab_layout .layout_icon li').last().children('a').addClass('animated fadeInDown');
@@ -77,28 +139,32 @@ $(document).ready(function() {
 		$(this).find('.tab_layout .layout_info').removeClass('animated fadeIn');
 	});
 
+	// анімація картинок "Оставить заявку"
 	$('.call_form input').focus(function() {
 		$('.call_icon img.active').css({
 			"-webkit-transform": "rotate(0deg)",
 			"-moz-transform": "rotate(0deg)",
-			"transform": "rotate(0deg)" /* For modern browsers(CSS3)  */
+			"transform": "rotate(0deg)"
 		}).removeClass('active');
-
 		$('.call_icon img').eq($(this).index()).css({
 			"-webkit-transform": "rotate(360deg)",
 			"-moz-transform": "rotate(360deg)",
-			"transform": "rotate(360deg)" /* For modern browsers(CSS3)  */
+			"transform": "rotate(360deg)"
 		}).addClass('active');
-
-		console.log($(this).index());
 	});
 
+	$('.li_dropdown').hover(function() {
+		$('.dropdown_menu').slideDown('500');
+	}, function() {
+		$('.dropdown_menu').slideUp('500');
+	});
 
 	$(window).resize(function() {
 		$('.service  .service_item').height(maxh);
 	});
 });
 
+//  функція додавання класу .active в елементах списку li
 function AddActive(elem){
 	$(elem).parent().siblings('li').removeClass('active');
 	$(elem).parent().addClass('active');
--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ gulp.task('scripts', function() {
         'app/libs/owl.carousel/dist/owl.carousel.min.js', //OwlCarousel
         'app/libs/jquery-fancyBox/dist/jquery.fancybox.min.js', //FancyBox
         'app/libs/jquery-mousewheel/jquery.mousewheel.min.js',
-        'app/libs/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js'
+        'app/libs/jquery-mixitup/dist/mixitup.min.js' //MixitUp
         ])
         .pipe(concat('libs.min.js')) // Собираем их в новом файле libs.min.js
         .pipe(uglify()) // Сжимаем JS файл
